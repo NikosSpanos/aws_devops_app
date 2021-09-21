1d1ee3cf937bd3953e50609b58516d66ed9d5055
NikosSpanos <spanos.nikolaos@outlook.com>
2021-09-21 13:51:52 +0300

    fixing errors to complete terraform planning v3

--- aws_production/modules/virtual_machine/variables.tf
+++ aws_production/modules/virtual_machine/variables.tf
@@ -2,9 +2,9 @@ variable "location" {
 	description = "Resource allocation zone in AWS"
 }
 
-variable "location_sg" {
-	description = "Resource allocation zone for Security Group (SG) in AWS"
-}
+# variable "location_sg" {
+# 	description = "Resource allocation zone for Security Group (SG) in AWS"
+# }
 
 variable "prefix" {
   description = "Resource group prefix (i.e development/ production)"
