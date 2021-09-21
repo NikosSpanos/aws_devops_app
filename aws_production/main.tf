1aaa2dc0fca6ab194e907263698b686665e4a639
NikosSpanos <spanos.nikolaos@outlook.com>
2021-09-21 14:05:44 +0300

    created new access credentials v2

--- aws_production/main.tf
+++ aws_production/main.tf
@@ -22,7 +22,7 @@ provider "aws"{
       access_key = var.aws_access_key
       secret_key = var.aws_secret_key
       //shared_credentials_file = var.credentials_path
-      profile = "default"
+      //profile = "default"
   }
 
 module "virtual_machines" {
