835c3e6883397b1a357b6473180ea28a1ae0a5db
NikosSpanos <spanos.nikolaos@outlook.com>
2021-09-22 15:50:52 +0300

    added access control list rules for ec2-instance v8

--- aws_production/modules/virtual_machine/main.tf
+++ aws_production/modules/virtual_machine/main.tf
@@ -199,19 +199,30 @@ resource "aws_instance" "production_server" {
 resource "aws_route_table" "route_table_prod" {
   vpc_id = aws_vpc.vpc_prod.id
 
-  route = [
-    {
-      cidr_block = "10.0.1.0/24"
-      gateway_id = aws_internet_gateway.gw.id
-      # instance_id = aws_instance.production_server.id
-      # network_interface_id = aws_network_interface.nic_prod.id
-    }
-  ]
+  # route = [
+  #   {
+  #     cidr_block = "10.0.1.0/24"
+  #     gateway_id = aws_internet_gateway.gw.id
+  #     instance_id = aws_instance.production_server.id
+  #     network_interface_id = aws_network_interface.nic_prod.id
+  #   }
+  # ]
 
   tags = {
     Name = "route table for production server"
   }
 }
+
+resource "aws_route" "route_prod" {
+  route_table_id         = aws_route_table.route_table_prod.id
+  destination_cidr_block = "10.0.1.0/24"
+  instance_id = aws_instance.production_server.id
+  network_interface_id = aws_network_interface.nic_prod.id
+  depends_on = [
+    aws_route_table.route_table_prod
+  ]
+}
+
 
 resource "aws_route_table_association" "table_association_prod" {
   subnet_id      = aws_subnet.subnet_prod.id
