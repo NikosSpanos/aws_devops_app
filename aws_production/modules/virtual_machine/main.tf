2614ba0cb8cd32dcaa32858de02a26294213ab63
NikosSpanos <spanos.nikolaos@outlook.com>
2021-09-22 10:49:51 +0300

    added association to public ip

--- aws_production/modules/virtual_machine/main.tf
+++ aws_production/modules/virtual_machine/main.tf
@@ -135,11 +135,12 @@ resource "aws_eip" "prod_server_public_ip" {
 # }
 
 resource "aws_instance" "production_server" {
-  ami               = data.aws_ami.ubuntu-server.id
-  instance_type     = "t2.micro"
-  key_name          = aws_key_pair.generated_key_prod.key_name
-  subnet_id         = aws_subnet.subnet_prod.id
-  vpc_security_group_ids = [aws_security_group.sg_prod.id]
+  ami                         = data.aws_ami.ubuntu-server.id
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.generated_key_prod.key_name
+  subnet_id                   = aws_subnet.subnet_prod.id
+  vpc_security_group_ids      = [aws_security_group.sg_prod.id]
+  associate_public_ip_address = true
 
   # network_interface {
   #   network_interface_id = aws_network_interface.nic_prod.id
