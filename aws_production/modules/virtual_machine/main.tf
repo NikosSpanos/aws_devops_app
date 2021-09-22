96511c1943f8d61eef49f1c88e8e30d7767f54ca
NikosSpanos <spanos.nikolaos@outlook.com>
2021-09-22 15:41:07 +0300

    added access control list rules for ec2-instance v6

--- aws_production/modules/virtual_machine/main.tf
+++ aws_production/modules/virtual_machine/main.tf
@@ -201,6 +201,8 @@ resource "aws_route_table" "route_table_prod" {
 
   route = [
     {
+      cidr_block = "10.0.1.0/24"
+      gateway_id = aws_internet_gateway.gw.id
       instance_id = aws_instance.production_server.id
       network_interface_id = aws_network_interface.nic_prod.id
     }
